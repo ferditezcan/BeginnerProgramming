91637b79cf5198777d2e4a2112bfaf62988a964f
ferditezcan <ferditezcan@users.noreply.github.com>
2018-07-25 15:22:18 +0300

    Function

---
+++ Function/Function/Function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void printCrap(int x) {
+	cout << "buckys favorite number is " << x << endl;
+}
+
+int main()
+{
+	printCrap(5);
+	cin.get();
+	return 0;
+}
